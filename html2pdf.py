314e137b3267c990157faa9927b14db0fe742a6a
Dovid Greenberger <dovidsgreenberger@gmail.com>
2022-05-30 22:33:31 -0400

    adding file to convert html to pdf

---
+++ html2pdf.py
@@ -0,0 +1,7 @@
+https://xhtml2pdf.readthedocs.io/en/latest/
+
+# use xhtml2pdf to convert html to pdf 
+# need to paginate the html by fitting the html between the header and footer
+# need to create a template pdf with the header and footer
+# add a page number to the footer
+# include project info in the header
